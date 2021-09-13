2a1e842c1d914a11ffa1cc08e111e878f33d13f9
Joe Chen <geometryolife@gmail.com>
2021-09-13 23:43:04 +0800

    feat(Chapter01/pra/t1, t2, record/e2): add
    Chapter01/pra/t1.cc: ...
    Chapter01/pra/t2_rewrite.cc: 改写返回值，观察运行结果。
    Chapter01/record/e2_io.cc: 两数相加程序。

---
+++ Chapter01/pra/t2_rewrite.cc
@@ -0,0 +1,20 @@
+// 练习 1.2: 改写程序，让它返回-1。返回值-1通常被当作程序错误的标识。
+// 重新编译并运行你的程序，观察你的系统如何处理 main 返回的错误标识。
+int main() { return -1; }
+
+/*
+>>> Execution Result:
+返回-1的输出:
+real    0m0.001s
+user    0m0.001s
+sys     0m0.000s
+
+[Process exited 255]
+
+返回零的输出:
+real    0m0.001s
+user    0m0.000s
+sys     0m0.000s
+
+[Process exited 0]
+ */
