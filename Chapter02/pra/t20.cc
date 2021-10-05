ff9329bf515c7e5c3a7f34c8ba5ad22900eebb97
Joe Chen <geometryolife@gmail.com>
2021-10-05 23:42:10 +0800

    feat(Chapter02/pra/t20.cc): add
    Chapter02/pra/t20.cc: 理解指针的底层操作。

---
+++ Chapter02/pra/t20.cc
@@ -0,0 +1,31 @@
+// 练习2.20: 请叙述下面这段代码的作用。
+
+// int i = 42;
+// int *p1 = &i;
+// *p1 = *p1 * *p1;
+
+#include <iostream>
+
+int main() {
+  int i = 42;   // 定义一个整型变量，初始化为 42
+  int *p1 = &i; // 定义一个 int 型指针 p1，指向变量 i
+
+  std::cout << p1 << std::endl;
+  std::cout << &i << std::endl;
+
+  // 通过解引用符，让 p1 指向的对象相乘，并把值赋给 p1 指向的对象
+  *p1 = *p1 * *p1;
+
+  std::cout << p1 << std::endl;
+  std::cout << *p1 << std::endl;
+
+  return 0;
+}
+
+/*
+>>> Execution Result:
+0x7ffc301a076c
+0x7ffc301a076c
+0x7ffc301a076c
+1764
+ */
